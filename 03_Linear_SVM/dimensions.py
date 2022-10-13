cd3d00c8ee2681d742ddb083af7e18b8dce36a23
MaciaKing <maciasalvasalva@gmail.com>
2022-10-13 14:38:05 +0200

    Pre classe

--- 03_Linear_SVM/dimensions.py
+++ 03_Linear_SVM/dimensions.py
@@ -4,29 +4,27 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-from Adaline import Adaline
-from sklearn.svm import SVC
 
+from sklearn.svm import SVC
 
 # Generació del conjunt de mostres
 X, y = make_classification(n_samples=400, n_features=5, n_redundant=0, n_repeated=0,
                            n_classes=2, n_clusters_per_class=1, class_sep=1,
                            random_state=9)
 
-# Separar les dades: train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-# Estandaritzar les dades: StandardScaler
+# Els dos algorismes es beneficien d'estandaritzar les dades
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+X_transformed = scaler.fit_transform(X_train)
+X_test_transformed = scaler.transform(X_test)
 
+#Entrenam una SVM linear (classe SVC)
+svm = SVC(C=1.0, kernel='linear')
+svm.fit(X_transformed, y_train)
+y_predicted = svm.predict(X_test_transformed)
 
-# Entrenam una SVM linear (classe SVC)
-perceptronSVC = SVC(kernel='linear') # Linear Kernel
-perceptronSVC.fit(X_train,y_train)
+differences = (y_predicted - y_test)
+errors = np.count_nonzero(differences)
 
-# Prediccio
-y_pred=perceptronSVC.predict(X_test)
-
-# Metrica
-# TODO
+print(f'Rati d\'acerts en el bloc de predicció: {(len(y_predicted)-errors)/len(y_predicted)}')
--- 03_Linear_SVM/main.py
+++ 03_Linear_SVM/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from Adaline import Adaline
 from sklearn.svm import SVC
@@ -9,9 +8,9 @@ from sklearn.svm import SVC
 
 # Generació del conjunt de mostres
 X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_repeated=0,
-                           n_classes=2, n_clusters_per_class=1, class_sep=2,
-                           random_state=9)
-y[y == 0] = -1
+                           n_classes=2, n_clusters_per_class=1, class_sep=0.7,
+                           random_state=8)
+y[y == 0] = -1  # La nostra implementació esta pensada per tenir les classes 1 i -1.
 
 # Els dos algorismes es beneficien d'estandaritzar les dades
 scaler = StandardScaler()
@@ -20,32 +19,26 @@ X_transformed = scaler.fit_transform(X)
 # Entrenam un perceptron
 perceptron = Adaline(eta=0.0005, n_iter=60)
 perceptron.fit(X_transformed, y)
-#y_prediction = perceptron.predict(X) no hago prediccion solo quiero la linea dibujada
-
-# TODO
-#X_trainFit = scaler.fit_transform(X_transformed)
+y_prediction = perceptron.predict(X)
 
 #Entrenam una SVM linear (classe SVC)
-perceptronSVC = SVC(C=0.1, kernel='linear') # Linear Kernel, C--> quan més alt, mes tolerant es a fallar.
-perceptronSVC.fit(X_transformed, y)
-#perceptronSVC.predict(X_test) no quiero predecir nada, solo dibujarlo
+svm = SVC(C=1000.0, kernel='linear')
+svm.fit(X_transformed, y)
 
 
 plt.figure(1)
-
 #  Mostram els resultats Adaline
 plt.scatter(X_transformed[:, 0], X_transformed[:, 1], c=y)
-m = -perceptron.w_[1] / perceptron.w_[2]  #Calcula la pendiente
+m = -perceptron.w_[1] / perceptron.w_[2]
 origen = (0, -perceptron.w_[0] / perceptron.w_[2])
 plt.axline(xy1=origen, slope=m, c="blue", label="Adaline")
 
 
 #  Mostram els resultats SVM
-# TODO
-m = -perceptronSVC.coef_[0][0] / perceptronSVC.coef_[0][1] #Calcula la pendiente
-origen = (0, -perceptronSVC.intercept_[0] / perceptronSVC.coef_[0][1])
-plt.axline(xy1=origen, slope=m, c="red", label="SVM")
-plt.scatter( perceptronSVC.support_vectors_[:,0], perceptronSVC.support_vectors_[:,1], facecolors="none", edgecolors="green")
+w = svm.coef_[0]
+a = -w[0] / w[1]
+plt.axline(xy1=(0, -svm.intercept_[0]/w[1]), slope=a, c="green", label="SVM")
+plt.scatter(svm.support_vectors_[:, 0], svm.support_vectors_[:, 1], facecolors="none", edgecolors="green")
 
 
 plt.legend()
